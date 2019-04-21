342cef709d692e518cdf3cc800fe91967f67db0d
Matt Tonks <tonks@chapman.edu>
2019-04-21 13:46:28 -0700

    Started genetics

--- genetics/genetics.js
+++ genetics/genetics.js
@@ -42,66 +42,29 @@ var GaussianDist = function(mean, std)
 
 function population ()
 {
-  this.n1 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-  this.n2 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-  this.n3 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-  this.n4 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-  this.n5 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-  this.n6 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-  this.n7 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-  this.n8 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-  this.n9 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-  this.n10 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
-
+  this.n1 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
+  this.n2 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
 
   this.mean = 0;
   this.std = 5;
 
 
-
+  this.setFitnessN1 = function (fit)
+  {
+    this.n1.fit = fit;
+  }
+  this.setFitnessN2 = function (fit)
+  {
+    this.n2.fit = fit;
+  }
   this.remove= function()
   {
-    var smallest = 0;
-    for(i = 0; i <= 9; i++)
+    if(this.n1.fit < this.n2.fit)
     {
-      var temp1 = this.getNextInd(i);
-      var temp2 = this.getNextInd(i + 1)
-      if(temp1.fit < temp2.fit)
-      {
-        smallest = i;
-      }
+      this.n1 = outNode outNode(GaussianDist(this.n2.a, 5), GaussianDist(this.n2.b, 5),GaussianDist(this.n2.c, 5),GaussianDist(this.n2.d, 5),GaussianDist(this.n2.e, 5),GaussianDist(this.n2.f, 5),GaussianDist(this.n2.node1.a, 5),GaussianDist(this.n2.node1.b, 5),GaussianDist(this.n2.node1.c, 5),GaussianDist(this.n2.node1.d, 5),GaussianDist(this.n2.node1.e, 5),GaussianDist(this.n2.node1.f, 5),GaussianDist(this.n2.node2.a, 5),GaussianDist(this.n2.node2.b, 5),GaussianDist(this.n2.node2.c, 5),GaussianDist(this.n2.node2.d, 5),GaussianDist(this.n2.node2.e, 5),GaussianDist(this.n2.node2.f, 5));
     }
-    if(smallest == 0)
-    {
-      this.n2;
-    }
-    else if (smallest == 1)
-    {
-      return this.n2;
-    }
-    else if (smallest == 2) {
-      return this.n3;
-    }
-    else if (smallest == 3) {
-      return this.n4;
-    }
-    else if (smallest == 4) {
-      return this.n5;
-    }
-    else if (smallest == 5) {
-      return this.n6;
-    }
-    else if (smallest == 6) {
-      return this.n7;
-    }
-    else if (smallest == 7) {
-      return this.n8;
-    }
-    else if (smallest == 8) {
-      return this.n9;
-    }
-    else if (smallest == 9) {
-      return this.n10;
+    else {
+      this.n2 = outNode outNode(GaussianDist(this.n1.a, 5), GaussianDist(this.n1.b, 5),GaussianDist(this.n1.c, 5),GaussianDist(this.n1.d, 5),GaussianDist(this.n1.e, 5),GaussianDist(this.n1.f, 5),GaussianDist(this.n1.node1.a, 5),GaussianDist(this.n1.node1.b, 5),GaussianDist(this.n1.node1.c, 5),GaussianDist(this.n1.node1.d, 5),GaussianDist(this.n1.node1.e, 5),GaussianDist(this.n1.node1.f, 5),GaussianDist(this.n1.node2.a, 5),GaussianDist(this.n1.node2.b, 5),GaussianDist(this.n1.node2.c, 5),GaussianDist(this.n1.node2.d, 5),GaussianDist(this.n1.node2.e, 5),GaussianDist(this.n1.node2.f, 5));
     }
   }
   this.getNextInd = function (inc)
@@ -113,30 +76,6 @@ function population ()
     else if (inc == 1)
     {
       return this.n2;
-    }
-    else if (inc == 2) {
-      return this.n3;
-    }
-    else if (inc == 3) {
-      return this.n4;
-    }
-    else if (inc == 4) {
-      return this.n5;
-    }
-    else if (inc == 5) {
-      return this.n6;
-    }
-    else if (inc == 6) {
-      return this.n7;
-    }
-    else if (inc == 7) {
-      return this.n8;
-    }
-    else if (inc == 8) {
-      return this.n9;
-    }
-    else if (inc == 9) {
-      return this.n10;
     }
   }
 }
--- genetics/pong-genetics.js
+++ genetics/pong-genetics.js
@@ -79,6 +79,8 @@ Ball.prototype.render = function () {
 var player = new Player();
 var computer = new Computer();
 var ball = new Ball(200, 300);
+var pop = new population();
+var fitness = 0;
 
 var render = function () {
 	context.fillStyle = "LightGray";
@@ -122,8 +124,7 @@ Ball.prototype.update = function (paddle1, paddle2) {
 		this.x_speed = 1;
 		this.y_speed = 3;
 		this.x = 200;
-		this.y = 300;
-		score++;
+		this.y = 300;// set fitness to the node switch nodes
 	}
 
 	if (top_y > 300) {
@@ -144,24 +145,8 @@ Ball.prototype.update = function (paddle1, paddle2) {
 	}
 };
 
-var keysDown = {};
-
-window.addEventListener("keydown", function (event) {
-	keysDown[event.keyCode] = true;
-});
-
-window.addEventListener("keyup", function (event) {
-	delete keysDown[event.keyCode];
-});
-
-var update = function () {
-	player.update();
-	ball.update(player.paddle, computer.paddle);
-};
-
 Player.prototype.update = function () {
-	for (var key in keysDown) {
-		var value = Number(key);
+	var value = pop.
 		if (value == 37) { // left arrow
 			this.paddle.move(-4, 0);
 		} else if (value == 39) { // right arrow
@@ -169,7 +154,7 @@ Player.prototype.update = function () {
 		} else {
 			this.paddle.move(0, 0);
 		}
-	}
+
 };
 
 Paddle.prototype.move = function (x, y) {
@@ -206,4 +191,87 @@ Computer.prototype.update = function (ball) {
 	} else if (this.paddle.x + this.paddle.width > 400) {
 		this.paddle.x = 400 - this.paddle.width;
 	}
+};
+function outNode(a, b,  var1, var2, var, var3, var4, var5, var6, var7, var8, var9, var10, var11, var12)
+{
+  this.a = a;
+  this.b = b;
+  this.node1 = node(var1, var2, var3, var4, var5, var6);
+  this.node2 = node(var7, var8, var9, var10, var11, var12);
+
+  this.fit;
+  this.getOutput = function()
+  {
+    return a * node1.outputNum() + b * node2.outputNum();
+  };
+  this.putFit = function(val)
+  {
+    this.fit = val;
+  };
+};
+function node(var1, var2, var3, var4, var5, var6) {
+  this.a = var1;
+  this.b = var2;
+  this.c = var3;
+  this.d = var4;
+  this.e = var5;
+  this.f = var6;
+
+  this.outputNum = function()
+  {
+    return a * Ball.x + b * Ball.y + c * Ball.x_speed + d * Ball.y_speed + e * Player.x + f * Player.y;
+  };
+}
+
+var GaussianDist = function(mean, std)
+{
+  var temp1 = Math.sqrt(2 * Math.exp(1.0 / Math.random()));
+  var temp2 = Math.cos(2 * 3.14 * Math.random());
+  return temp1 * temp2;
+}
+
+function population ()
+{
+  this.n1 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
+  this.n2 = outNode(GaussianDist(0, 5), GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5),GaussianDist(0, 5));
+	this.node = 0;
+	this.getOutNode = function ()
+	{
+		if (this.node == 0)
+		{
+			return this.n1;
+		}
+		return this.n2;
+	};
+  	this.setFitnessN1 = function (fit)
+  {
+    this.n1.fit = fit;
+		this.node = 1;
+  };
+  this.setFitnessN2 = function (fit)
+  {
+    this.n2.fit = fit;
+		this.node = 0;
+  };
+  this.remove= function()
+  {
+    if(this.n1.fit < this.n2.fit)
+    {
+      this.n1 = outNode outNode(GaussianDist(this.n2.a, 5), GaussianDist(this.n2.b, 5),GaussianDist(this.n2.c, 5),GaussianDist(this.n2.d, 5),GaussianDist(this.n2.e, 5),GaussianDist(this.n2.f, 5),GaussianDist(this.n2.node1.a, 5),GaussianDist(this.n2.node1.b, 5),GaussianDist(this.n2.node1.c, 5),GaussianDist(this.n2.node1.d, 5),GaussianDist(this.n2.node1.e, 5),GaussianDist(this.n2.node1.f, 5),GaussianDist(this.n2.node2.a, 5),GaussianDist(this.n2.node2.b, 5),GaussianDist(this.n2.node2.c, 5),GaussianDist(this.n2.node2.d, 5),GaussianDist(this.n2.node2.e, 5),GaussianDist(this.n2.node2.f, 5));
+    }
+    else {
+      this.n2 = outNode outNode(GaussianDist(this.n1.a, 5), GaussianDist(this.n1.b, 5),GaussianDist(this.n1.c, 5),GaussianDist(this.n1.d, 5),GaussianDist(this.n1.e, 5),GaussianDist(this.n1.f, 5),GaussianDist(this.n1.node1.a, 5),GaussianDist(this.n1.node1.b, 5),GaussianDist(this.n1.node1.c, 5),GaussianDist(this.n1.node1.d, 5),GaussianDist(this.n1.node1.e, 5),GaussianDist(this.n1.node1.f, 5),GaussianDist(this.n1.node2.a, 5),GaussianDist(this.n1.node2.b, 5),GaussianDist(this.n1.node2.c, 5),GaussianDist(this.n1.node2.d, 5),GaussianDist(this.n1.node2.e, 5),GaussianDist(this.n1.node2.f, 5));
+    }
+  };
+  this.getNextInd = function (inc)
+  {
+    if(inc == 0)
+    {
+      return this.n1;
+    }
+    else if (inc == 1)
+    {
+      return this.n2;
+    }
+  };
 };
